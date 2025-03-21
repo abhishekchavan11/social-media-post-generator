ffd03ee97e277cf05dc20b0a41cab4e5a1394fed
abhishekchavan11 <AbhishekC11@hexaware.com>
2025-03-21 17:31:33 +0530

    changes made for adding new tabs and some css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
@@ -29,6 +30,7 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
     ReactiveFormsModule,
     HttpClientModule,
     CarouselModule,
+    MatTabsModule,
     MatInputModule,MatFormFieldModule,MatProgressSpinnerModule,MatButtonModule, MatIconModule,MatToolbarModule,MatTooltipModule
   ],
   providers: [],
